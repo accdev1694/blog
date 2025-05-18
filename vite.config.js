300300b3dfe6d6fc82b14266f02d539f044e6526
unknown <diloch111@gmail.com>
2025-05-18 03:26:24 +0100

    update vite config base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // Add this line - should match your repository name
+  base: "/blog", // Add this line - should match your repository name
   build: {
     outDir: "dist",
   },
